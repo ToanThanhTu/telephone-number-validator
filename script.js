1135febbdcab167149af4763b705ba9616d189b8
Trevor <toantu3005@gmail.com>
2024-06-08 19:38:26 +1000

    Initial

---
+++ script.js
@@ -0,0 +1,18 @@
+const num1Btn = document.getElementById("num1");
+const num2Btn = document.getElementById("num2");
+const num3Btn = document.getElementById("num3");
+const num4Btn = document.getElementById("num4");
+const num5Btn = document.getElementById("num5");
+const num6Btn = document.getElementById("num6");
+const num7Btn = document.getElementById("num7");
+const num8Btn = document.getElementById("num8");
+const num9Btn = document.getElementById("num9");
+const num0Btn = document.getElementById("num0");
+const openParenthesisBtn = document.getElementById("open-parenthesis");
+const closeParenthesisBtn = document.getElementById("close-parenthesis");
+
+const userInput = document.getElementById("user-input");
+
+num0Btn.addEventListener("click", () => {
+    console.log("hello");
+});
